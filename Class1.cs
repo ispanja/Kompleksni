98ac2c2bdcd8d0d6130f03f5dde35f3f22967bd0
Ivan Spanja <IvanSp@dspace.hr>
2021-10-21 18:16:22 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kompleksni
+{
+    class KompleksniBrojKlasa
+    {
+        public KompleksniBrojKlasa(double realni, double imaginarni) {
+            RealniDio = realni;
+            ImaginarniDio = imaginarni;
+        }
+
+        public double RealniDio;
+        public double ImaginarniDio;
+
+        public override string ToString()
+        {
+            return $"({RealniDio}, {ImaginarniDio})";
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kompleksni
+{
+    struct KompleksniBrojStruktura {
+
+        public double RealniDio;
+        public double ImaginarniDio;
+
+        public KompleksniBrojStruktura(double realni, double imaginarni)
+        {
+            RealniDio = realni;
+            ImaginarniDio = imaginarni;
+        }
+
+        public override string ToString()
+        {
+            return $"({RealniDio}, {ImaginarniDio})";
+        }
+    }
+}
+
